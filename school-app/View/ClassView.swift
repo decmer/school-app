01a8ba1476c591236f4b2aac741ec5627a8e32ef
jose <jose.decena@icloud.com>
2024-11-20 16:02:25 +0100

    commit, Test

---
+++ school-app/View/ClassView.swift
@@ -0,0 +1,18 @@
+//
+//  ClassView.swift
+//  school-app
+//
+//  Created by Jose Merino Decena on 20/11/24.
+//
+
+import SwiftUI
+
+struct ClassView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ClassView()
+}
