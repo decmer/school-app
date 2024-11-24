c66679ee3620c58396676d399cd4e4b6ff9649dc
jose <jose.decena@icloud.com>
2024-11-24 19:52:12 +0100

    Commit añadiendo funcionalidades

---
+++ school-app/View/Admin/UsersView.swift
@@ -0,0 +1,18 @@
+//
+//  UsersView.swift
+//  school-app
+//
+//  Created by Jose Merino Decena on 24/11/24.
+//
+
+import SwiftUI
+
+struct UsersView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UsersView()
+}
